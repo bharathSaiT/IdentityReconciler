e178390e0e0a360453651b1ab1c13d7c5f9f221c
Bharath Sai <53166664+bharathSaiT@users.noreply.github.com>
2023-07-17 19:56:03 +0530

    enhanced 'findPrimaryContact' to get the primaryContact in all cases , including updation and creation

--- src/db/db_accessor.ts
+++ src/db/db_accessor.ts
@@ -25,6 +25,24 @@ import {client} from './db_client';
             };
             const result = await client.query(primaryIdQuery);
           
+          // Return the primaryContact object
+            if(result.rows.length === 0) 
+            {
+              console.info("created a new primary contact");
+              return createPrimaryContact(email,phoneNumber);
+            }
+            else if (result.rows.length > 1) {
+              console.info("update the primary contacts to secondary based on the age of the data ");
+                return updatePrimaryToSecondary(result);
+            }
+            console.info("fetched primary id using the linkedId of secondary contact");
+            return result.rows[0];
+          
+      }
+      else if (result.rows.length > 1) {
+          // Update the primary contacts to secondary contacts and return the primary contact
+          console.info("update the primary contacts to secondary based on the age of the data ");
+          return updatePrimaryToSecondary(result);
       }
       // Return the primaryContact object
       return result.rows[0];
@@ -33,7 +51,7 @@ import {client} from './db_client';
       console.error('Error executing query:', error);
     }
   };
-/**
+  /**
    * Function to fetch all the secondary contacts linked to the email & phoneNumber
    * @param email 
    * @param phoneNumber 
@@ -112,3 +130,36 @@ import {client} from './db_client';
       console.error('Error executing query:', error);
     }
   };
+  /**
+   * Function to update primary contacts to seconday contacts
+   * @param result : Array of primary contacts
+   * @returns : the og primary contact
+   */
+  const updatePrimaryToSecondary = async (result : any): Promise<any> => {
+  
+      //sort in ascending order of date 
+      result.rows.sort((a: { createdat: string | number | Date; }, b: { createdat: string | number | Date; }) => new Date(a.createdat).getTime() - new Date(b.createdat).getTime());
+      const updatedObjects = result.rows.slice(1);
+  
+      //get the primaryId , by getting the oldest created contact record
+      const primaryId = result.rows[0].id;
+      
+      //update all the elements ,linkprecedence to secondary and linkedId from null to primaryId
+      const modifyPrimary = {
+          text: `UPDATE contact SET linkprecedence = $1 ,linkedid=$2 WHERE id IN (${updatedObjects.map((obj: { id: any; }) => obj.id).join(', ')})`,
+          values: ['secondary' ,primaryId]
+      };
+      await client.query(modifyPrimary);
+  
+      // Update the linkedId for all the elements whose current linkedId was among the above updates contacts
+      const updateLinkedId ={
+          text: `UPDATE contact SET linkedid = $1 WHERE linkedid IN (${updatedObjects.map((obj: { id: any; }) => obj.id).join(', ')})`,
+          values: [primaryId]
+      }
+      await client.query(updateLinkedId);
+  
+      //return the primaryContact object
+      return result.rows[0];
+  };
+
+export { findPrimaryContact , findSecondaryContacts, createPrimaryContact, createSecondaryContact, updatePrimaryToSecondary};
